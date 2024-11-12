cd09ea8fdc8b61d2bd75a61d7a566123a3ff8c59
akmalmzkki <akmalmzkki@student.telkomuniversity.ac.id>
2024-11-12 23:34:45 +0700

    .

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -61,25 +61,22 @@ class ConvWithKAN(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
 
     default_act = nn.SiLU()  # default activation
-    # other activation functions can be used
-    # default_act = AGLU()
 
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
         self.conv = FastConvKAN(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d)
         self.bn = nn.BatchNorm2d(c2)
-        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
     def forward(self, x):
         """Apply convolution, batch normalization to input tensor."""
-        return self.act(self.bn(self.conv(x)))
-        # return self.bn(self.conv(x))
+        # return self.act(self.bn(self.conv(x)))
+        return self.bn(self.conv(x))
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
-        return self.act(self.conv(x))
-        # return self.conv(x)
+        # return self.act(self.conv(x))
+        return self.conv(x)
 
 class Conv2(Conv):
     """Simplified RepConv module with Conv fusing."""
